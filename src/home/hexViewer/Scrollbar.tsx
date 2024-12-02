7107bd9894433a8d05b9535fedfec05b7854f16e
Mykhailo Mozghovoi <mvmlisb@gmail.com>
2024-12-02 23:12:31 +0200

    In progress

--- src/home/hexViewer/Scrollbar.tsx
+++ src/home/hexViewer/Scrollbar.tsx
@@ -3,6 +3,7 @@ import React, {useEffect, useRef} from "react";
 
 export default function Scrollbar() {
     const thumbRef = React.useRef<HTMLDivElement>(null);
+    const thumbRootRef = React.useRef<HTMLDivElement>(null);
     const isMouseDownRef = useRef<boolean>(false);
     const [thumbY, setThumbY] = React.useState<number>(0);
 
@@ -34,13 +35,12 @@ export default function Scrollbar() {
         };
     }, []);
 
+    console.log("new pos", Math.max(0, thumbY));
+
+    console.log(JSON.stringify(thumbRef.current?.getBoundingClientRect()));
+
     return (
-        <Column
-            style={{
-                height: "100%",
-                position: "relative"
-            }}
-        >
+        <Column>
             <div
                 style={{
                     height: "8px",
@@ -49,28 +49,41 @@ export default function Scrollbar() {
                     border: "1px solid black"
                 }}
             />
+            <Column
+                style={{
+                    height: "100%",
+                    position: "relative"
+                }}
+                ref={thumbRootRef}
+            >
+                <div
+                    ref={thumbRef}
+                    style={{
+                        height: "8px",
+                        position: "relative",
+                        width: "8px",
+                        border: "1px solid lightgray",
+                        top: Math.min(
+                            Math.max(0, thumbY),
+                            (thumbRootRef.current?.getBoundingClientRect()
+                                .bottom ?? 0) +
+                                (thumbRef.current?.getBoundingClientRect()
+                                    .height ?? 0)
+                        )
+                    }}
+                    onMouseDown={() => {
+                        console.log("mouse down");
+                        isMouseDownRef.current = true;
+                    }}
+                />
+            </Column>
             <div
-                ref={thumbRef}
                 style={{
                     height: "8px",
+                    width: "8px",
                     position: "relative",
-                    width: "8px",
-                    border: "1px solid lightgray",
-                    top: Math.max(0, thumbY)
-                }}
-                onMouseDown={() => {
-                    console.log("mouse down");
-                    isMouseDownRef.current = true;
-                }}
-            />
-
-            <div
-                style={{
-                    height: "8px",
-                    width: "8px",
-                    position: "absolute",
                     border: "1px solid black",
-                    bottom: "0"
+                    top: "0"
                 }}
             />
         </Column>
